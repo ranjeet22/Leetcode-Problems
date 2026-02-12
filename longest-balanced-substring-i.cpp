505bc6f3d217c4c72eab01e89b3615c232b02d43
Ranjeet Singh <68098838+ranjeet22@users.noreply.github.com>
2026-02-12 21:57:26 +0530

    Add files via upload

---
+++ longest-balanced-substring-i.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool checkBalance(vector<int> &freq){
+        int common = 0;
+        for(int i=0;i<26;i++){
+            if(freq[i]==0) continue;
+            if(common == 0) common = freq[i];
+            else if(freq[i] != common) return false;
+        }
+        return true;
+    }
+
+    int longestBalanced(string s) {
+        int n = s.length();
+        int maxlen = 0;
+        for(int i=0;i<n;i++){
+            vector<int> freq(26, 0);
+            for(int j=i;j<n;j++){
+                freq[s[j]-'a']++;
+                if(checkBalance(freq)) maxlen = max(maxlen, j-i+1);
+            }
+        }
+        return maxlen;
+    }
+};
