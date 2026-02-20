fad1653151695e7a605e2d557d39c558a505425a
Ranjeet Singh <68098838+ranjeet22@users.noreply.github.com>
2026-02-20 22:27:19 +0530

    Add files via upload

---
+++ special-binary-string.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string makeLargestSpecial(string s) {
+        vector<string> special;
+        int start = 0;
+        int sum = 0;
+        for(int i=0;i<s.length();i++){
+            sum += (s[i] == '1')? 1 : -1;
+            if(sum == 0){
+                string inner = s.substr(start+1, i-start-1);
+                special.push_back("1" + makeLargestSpecial(inner) + "0");
+                start = i+1;
+            }
+        }
+        sort(begin(special), end(special), greater<string>()); // descending order
+        string result;
+        for(string &str : special) result += str;
+        return result;
+    }
+};
