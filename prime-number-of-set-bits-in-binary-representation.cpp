1b78733b698df87b83d53dbda509a11fd455f647
Ranjeet Singh <68098838+ranjeet22@users.noreply.github.com>
2026-02-21 13:58:01 +0530

    Add files via upload

---
+++ prime-number-of-set-bits-in-binary-representation.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countPrimeSetBits(int left, int right) {
+        unordered_set<int> primes = {2,3,5,7,11,13,17,19};
+        int count = 0;
+
+        for(int i=left;i<=right;i++){
+            int setbits = __builtin_popcount(i); // used to count set bits
+            if(primes.count(setbits)) count++;
+        }
+        return count;
+    }
+};
