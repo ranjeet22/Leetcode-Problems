90b248e03165ba7ee35e01ce5658dfc531c43b50
Ranjeet Singh <68098838+ranjeet22@users.noreply.github.com>
2026-02-28 22:27:33 +0530

    Add files via upload

---
+++ concatenation-of-consecutive-binary-numbers.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int M = 1e9+7;
+    int concatenatedBinary(int n) {
+        long result = 0;
+        for(int i=1;i<=n;i++){
+            int digits = log2(i) + 1;
+            result = ((result<<digits)%M + i)%M;
+        }
+        return result;
+    }
+};
